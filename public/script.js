90eb392195b3892ad5cd35a902e2d3ff68b9d4a3
LitGrizzly <jervaanhabib22@gmail.com>
2025-04-15 20:17:47 -0400

    Project Commit 1

--- public/script.js
+++ public/script.js
@@ -9,3 +9,38 @@ const options = {
 		'x-rapidapi-host': config.rapidApi.host
 	}
 };
+
+function showMovies(movies) {
+    let grid = document.querySelector('#container');
+    let html = '';
+    const limitedMovies = movies.slice(0, 100);
+
+
+    for (let i = 0; i < limitedMovies.length; i++) {
+        const movie = limitedMovies[i];
+
+        // Use the movie's index as its ID if it doesn't have one
+        const movieId = movie.id || 'movie_' + i;
+        
+        html += `
+            <div class="movie-card" data-id="${movieId}">
+                <img class="movie-pic" src="${movie.primaryImage}" alt="">
+                <div class="movie-info-box">
+                    <div class="movie-info">
+                        <h3>${movie.primaryTitle}</h3>
+                        <p>${movie.genres[0]}</p>
+                    </div>
+                    <div class="movie-card-buttons">
+                        <button class="like-btn" data-id="${movieId}">Like</button>
+                        <button class="watchlist-btn" data-id="${movieId}">Watchlist</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+  
+    grid.innerHTML = html;
+    
+    // Add click event listeners to all movie cards
+    addMovieCardClickListeners();
+}
